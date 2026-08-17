488ae860f7cf8d9bde33d52e364ae2af3bbd308e
CastellPg <reinaldoantoniocastellanoneto@gmail.com>
2026-08-17 14:31:41 -0300

    iniciar-angular: inicio do projeto angular com lint e roteamento basico

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,44 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login').then(
+        (m) => m.Login,
+      ),
+  },
+  {
+    path: 'cliente',
+    loadComponent: () =>
+      import('./features/cliente/pages/home/home').then(
+        (m) => m.Home,
+      ),
+  },
+  {
+    path: 'gerente',
+    loadComponent: () =>
+      import('./features/gerente/pages/home/home').then(
+        (m) => m.Home,
+      ),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./features/admin/pages/home/home').then(
+        (m) => m.Home,
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/pages/not-found/not-found').then(
+        (m) => m.NotFound,
+      ),
+  },
+];
